79a2ecc9019ae454531dfb88f66a57cefd6aa3d1
Victor Avdeev <avdeevvictor95@gmail.com>
2020-10-06 03:12:44 +0300

    feat: add morse decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  var arr = expr.match(/.{1,10}/g);
+  var decodedMessage = '';
+  arr.forEach(function(value, index){
+   switch (value.replace(/00/g, "").replace(/10/g, ".").replace(/11/g, "-")){
+      case '.-'     : decodedMessage = decodedMessage + 'a'; break;
+      case '-...'   : decodedMessage = decodedMessage + 'b'; break;
+      case '-.-.'   : decodedMessage = decodedMessage + 'c'; break;
+      case '-..'    : decodedMessage = decodedMessage + 'd'; break;
+      case '.'      : decodedMessage = decodedMessage + 'e'; break;
+      case '..-.'   : decodedMessage = decodedMessage + 'f'; break;
+      case '--.'    : decodedMessage = decodedMessage + 'g'; break;
+      case '....'   : decodedMessage = decodedMessage + 'h'; break;
+      case '..'     : decodedMessage = decodedMessage + 'i'; break;
+      case '.---'   : decodedMessage = decodedMessage + 'j'; break;
+      case '-.-'    : decodedMessage = decodedMessage + 'k'; break;
+      case '.-..'   : decodedMessage = decodedMessage + 'l'; break;
+      case '--'     : decodedMessage = decodedMessage + 'm'; break;
+      case '-.'     : decodedMessage = decodedMessage + 'n'; break;
+      case '---'    : decodedMessage = decodedMessage + 'o'; break;
+      case '.--.'   : decodedMessage = decodedMessage + 'p'; break;
+      case '--.-'   : decodedMessage = decodedMessage + 'q'; break;
+      case '.-.'    : decodedMessage = decodedMessage + 'r'; break;
+      case '...'    : decodedMessage = decodedMessage + 's'; break;
+      case '-'      : decodedMessage = decodedMessage + 't'; break;
+      case '..-'    : decodedMessage = decodedMessage + 'u'; break;
+      case '...-'   : decodedMessage = decodedMessage + 'v'; break;
+      case '.--'    : decodedMessage = decodedMessage + 'w'; break;
+      case '-..-'   : decodedMessage = decodedMessage + 'x'; break;
+      case '-.--'   : decodedMessage = decodedMessage + 'y'; break;
+      case '--..'   : decodedMessage = decodedMessage + 'z'; break;
+      case '.----'  : decodedMessage = decodedMessage + '1'; break;
+      case '..---'  : decodedMessage = decodedMessage + '2'; break;
+      case '...--'  : decodedMessage = decodedMessage + '3'; break;
+      case '....-'  : decodedMessage = decodedMessage + '4'; break;
+      case '.....'  : decodedMessage = decodedMessage + '5'; break;
+      case '-....'  : decodedMessage = decodedMessage + '6'; break;
+      case '--...'  : decodedMessage = decodedMessage + '7'; break;
+      case '---..'  : decodedMessage = decodedMessage + '8'; break;
+      case '----.'  : decodedMessage = decodedMessage + '9'; break;
+      case '-----'  : decodedMessage = decodedMessage + '0'; break;
+      default       : decodedMessage = decodedMessage + ' '; break;
+   }
+ }
+);
+return decodedMessage;
 }
 
 module.exports = {
